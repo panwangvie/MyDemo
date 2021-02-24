05fb9547fcb7e0e060f2c1a582be9e9d2749589a
suyunxiang_Hub88 <suyunxiang@gktel.com.cn>
2021-02-24 20:49:52 +0800

    多路绑定Demo

---
+++ BindingDemo/CustomMultiValueConvertor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BindingDemo
+{
+    public class CustomMultiValueConvertor : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                if (0.Equals(values[0]))
+                {
+                    return "--";
+                }
+                else
+                {
+                     
+                    string result = values[1].ToString()+ values[0];
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "--";
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (value as string).Split(' ');
+        }
+    }
+}
